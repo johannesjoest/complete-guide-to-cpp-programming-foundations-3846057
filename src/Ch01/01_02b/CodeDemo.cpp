550842ecb08a472453940ec45b9c16bb680ead0b
Johannes Jöst <johannes.joest@wolterskluwer.com>
2025-03-20 13:42:30 +0100

    Entwicklungsumgebnung + Erstes Hallo Welt Programm

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,22 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <cstdio>
+#include <iostream>
+
+int helloworld(){
+    std::cout <<"Hallo Welt!";
+    return 0;
+}
+
+int main(){
+   std::cout << "-----------ANFANG-------------" << std::endl;
+   std::cout << std::endl;
+
+   helloworld();
+
+   std::cout << std::endl << std::endl;
+   std::cout << "----------ENDE--------------" << std::endl;
+   return 0;
+}
